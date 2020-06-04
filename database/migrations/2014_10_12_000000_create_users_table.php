0f23e5da9cf611272ff9ec06ed1a6b22fe8e3720
Aditya Chamim Pratama <pratamaditya7@gmail.com>
2020-06-04 18:29:28 +0700

    add admin & approved in user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,8 +17,9 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('role')->nullable();
             $table->string('avatar')->default('assets/manage/images/default-user.png');
+            $table->boolean('admin')->default(false);
+            $table->timestamp('approved_at')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
