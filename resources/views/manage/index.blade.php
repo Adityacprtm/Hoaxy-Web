10fb032ab281e25ec814fd07751e85b910bc60af
Aditya Chamim Pratama <pratamaditya7@gmail.com>
2020-08-06 18:08:50 +0800

    cleanup assets manage

--- resources/views/manage/index.blade.php
+++ resources/views/manage/index.blade.php
@@ -2,9 +2,6 @@
 @section('title', 'Dashboard')
 
 @push('css')
-{{-- <link href="{{ asset('assets/manage/plugins/apex/apexcharts.css') }}" rel="stylesheet" type="text/css"> --}}
-{{-- <link href="{{ asset('assets/manage/assets/css/dashboard/dash_1.css') }}" rel="stylesheet" type="text/css" class="dashboard-analytics" /> --}}
-
 <style>
 	.center {
 		position: absolute;
@@ -20,24 +17,16 @@
 @endpush
 
 @section('content')
-<!--  BEGIN CONTENT AREA  -->
 <div id="content" class="main-content">
 	<div class="layout-px-spacing">
-		{{-- <div class="row layout-top-spacing"> --}}
 		<h1 class="center"><i class="fas fa-door-open"></i> W e l c o m e .</h1>
-		{{-- </div> --}}
 	</div>
 
 	@include('manage/includes/footer');
 </div>
-<!--  END CONTENT AREA  -->
 @endsection
 
 @push('js')
-<!-- BEGIN PAGE LEVEL PLUGINS/CUSTOM SCRIPTS -->
-{{-- <script src="{{ asset('assets/manage/plugins/apex/apexcharts.min.js') }}"></script> --}}
-{{-- <script src="{{ asset('assets/manage/assets/js/dashboard/dash_1.js') }}"></script> --}}
-<!-- BEGIN PAGE LEVEL PLUGINS/CUSTOM SCRIPTS -->
 <script>
 	$('#menu-dashboard').addClass('active');
     $('#menu-dashboard a').attr('data-active','true');
