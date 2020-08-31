fb4c672b9402f84e3b95f71f38347825d24cd66c
Aditya Chamim Pratama <pratamaditya7@gmail.com>
2020-08-31 21:29:10 +0700

    icon mbeb

--- resources/views/mbeb/index.blade.php
+++ resources/views/mbeb/index.blade.php
@@ -3,7 +3,11 @@
 <head>
 	<title>Hi mbeb!</title>
 
-	<link rel="icon" href="{{ asset('assets/mbeb/image/icon.png') }}">
+	{{-- <link rel="icon" href="{{ asset('assets/mbeb/image/icon.png') }}"> --}}
+	<link rel="shortcut icon" href="{{ asset('assets/mbeb/image/icon.png') }}">
+	<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('assets/mbeb/image/icon.png') }}">
+	<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/mbeb/image/icon.png') }}">
+	<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/mbeb/image/icon.png') }}">
 	<link rel="stylesheet" href="{{ asset('assets/mbeb/css/style.css') }}">
 </head>
 
