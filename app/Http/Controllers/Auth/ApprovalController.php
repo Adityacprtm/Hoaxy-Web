da686f082c26bac4d37612bdbd0bd937e617c7ed
Aditya Pratama <pratamaditya7@gmail.com>
2020-10-01 12:56:11 +0800

    🔒 hash password

--- app/Http/Controllers/Auth/ApprovalController.php
+++ app/Http/Controllers/Auth/ApprovalController.php
@@ -7,8 +7,8 @@ use Illuminate\Support\Facades\Auth;
 
 class ApprovalController extends Controller
 {
-    public function index()
-    {
-        return view('auth/approval');
-    }
+	public function index()
+	{
+		return view('auth/approval');
+	}
 }
