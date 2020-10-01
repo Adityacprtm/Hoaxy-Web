507ed9fd8fc8481c0e027ae6f25f531303d9b429
Aditya Pratama <pratamaditya7@gmail.com>
2020-10-01 12:37:59 +0800

    ✨ add image caption hover on portfolio

--- resources/views/main/portfolio.blade.php
+++ resources/views/main/portfolio.blade.php
@@ -1,6 +1,7 @@
 @extends('main.layouts.default')
 @section('title', 'Portfolio')
-@section('description', 'Adityacprtm.com is a personal website on behalf of Aditya Chamim Pratama which contains portfolio, blog and owner information. This page lists my portfolio.')
+@section('description', 'Adityacprtm.com is a personal website on behalf of Aditya Chamim Pratama which contains
+portfolio, blog and owner information. This page lists my portfolio.')
 
 @section('content')
 <div class="box box-content">
@@ -8,20 +9,21 @@
 		<h1 class="title title--h1 first-title title__separate">Portfolio</h1>
 	</div>
 
-	<!-- Filter -->
+	{{-- Filter --}}
 	<div class="select">
 		<span class="placeholder">Select category</span>
 		<ul class="filter">
 			<li class="filter__item">Category</li>
 			<li class="filter__item active" data-filter="*"><a class="filter__link active" href="#filter">All</a></li>
 			@foreach ($category as $c)
-			<li class="filter__item" data-filter=".category-{{ $c->category_name }}"><a class="filter__link" href="#filter">{{ $c->category_name }}</a></li>
+			<li class="filter__item" data-filter=".category-{{ $c->category_name }}"><a class="filter__link"
+					href="#filter">{{ $c->category_name }}</a></li>
 			@endforeach
 		</ul>
 		<input type="hidden" name="changemetoo" />
 	</div>
 
-	<!-- Gallery -->
+	{{-- Gallery --}}
 	<div class="gallery-grid gallery-grid-two js-grid js-filter-container">
 		<div class="gutter-sizer"></div>
 
@@ -29,6 +31,13 @@
 		<figure class="gallery-grid__item category-{{ $p->categoryPortfolio->category_name ?? '' }}">
 			<div class="gallery-grid__image-wrap">
 				<img class="gallery-grid__image cover lazyload" src="{{ asset($p->media) }}" data-zoom alt="">
+				<div class="caption">
+					<div class="blur"></div>
+					<div class="caption-text">
+						<h1>Tech Stack</h1>
+						<p>MongoDB - Express - Vue - Nodejs</p>
+					</div>
+				</div>
 			</div>
 			<figcaption class="gallery-grid__caption">
 				<h4 class="title title--h4 gallery-grid__title">{{ $p->title }}</h4>
@@ -38,6 +47,57 @@
 		</figure>
 		@endforeach
 
-	</div><!-- Gallery End -->
-</div>
-@endsection
+	</div>
+	@endsection
+
+	@push('css')
+
+	{{-- Image Caption Hover --}}
+	<style>
+		.gallery-grid__item:hover .caption {
+			opacity: 1;
+			transform: translateY(-100px);
+			-webkit-transform: translateY(-100px);
+			-moz-transform: translateY(-100px);
+			-ms-transform: translateY(-100px);
+			-o-transform: translateY(-100px);
+		}
+
+		.caption {
+			cursor: pointer;
+			position: absolute;
+			/* opacity: 0; */
+			/* top: 100%; */
+			-webkit-transition: all 0.15s ease-in-out;
+			-moz-transition: all 0.15s ease-in-out;
+			-o-transition: all 0.15s ease-in-out;
+			-ms-transition: all 0.15s ease-in-out;
+			transition: all 0.15s ease-in-out;
+			width: 100%
+		}
+
+		.blur {
+			background-color: rgba(0, 0, 0, 0.7);
+			height: 200px;
+			width: 100%;
+			z-index: 5;
+			/* position: absolute; */
+		}
+
+		.caption-text h1 {
+			color: #fff;
+			text-transform: uppercase;
+			font-size: 18px;
+		}
+
+		.caption-text {
+			z-index: 10;
+			color: #fff;
+			position: absolute;
+			width: 100%;
+			/* height: 300px; */
+			text-align: center;
+			top: 20px;
+		}
+	</style>
+	@endpush
