a29858f445d7c500cce578b82624f8a39cc6efd9
Aditya Pratama <pratamaditya7@gmail.com>
2020-10-03 12:56:58 +0800

    💄 add mapbox dark style

--- public/assets/main/js/mapbox.init.js
+++ public/assets/main/js/mapbox.init.js
@@ -1,28 +1,36 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoiYXJ0dGVtcGxhdGUiLCJhIjoiY2s0M3I5ZHgzMGEzNDNucXM1cDd0dzl3cSJ9.a2wjLlxz8LzWj9nIoGsshw';
+var theme = "light";
+const userPrefersDark =
+	window.matchMedia &&
+	window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+if (userPrefersDark) {
+	theme = "dark";
+}
+
+mapboxgl.accessToken =
+	"pk.eyJ1IjoiYWRpdHlhY3BydG0iLCJhIjoiY2thd2JvcWxnMjJuODJxbXh2cWY5dHl3ayJ9.Wnk9lMcXHr09UdQ-Iicp9Q";
 var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/light-v10',
-	center: [-73.9751,40.7289], // starting position
-    zoom: 13 // starting zoom
+	container: "map",
+	style: "mapbox://styles/mapbox/" + theme + "-v10",
+	center: [114.74047295804884, -3.42994542196984], // starting position
+	zoom: 13 // starting zoom
 });
-		
+
 // create the popup
-var popup = new mapboxgl.Popup({ offset: 40 }).setText(
-    '756 Livingston Street, Brooklyn, NY 11201'
-);
+var popup = new mapboxgl.Popup({ offset: 40 }).setText("Banjarbaru, Indonesia");
 
 // create DOM element for the marker
-var el = document.createElement('div');
-el.id = 'marker';
- 
+var el = document.createElement("div");
+el.id = "marker";
+
 // create the marker
 new mapboxgl.Marker(el)
-    .setLngLat([-73.9751,40.7289])
-    .setPopup(popup) // sets a popup on this marker
-    .addTo(map);
+	.setLngLat([114.74047295804884, -3.42994542196984])
+	.setPopup(popup) // sets a popup on this marker
+	.addTo(map);
 
 // Add zoom and rotation controls to the map.
-map.addControl(new mapboxgl.NavigationControl(), 'bottom-right');
+map.addControl(new mapboxgl.NavigationControl(), "bottom-right");
 
 // disable map zoom when using scroll
 map.scrollZoom.disable();
