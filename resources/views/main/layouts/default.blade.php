9a1c710dc5ad122f9ce8d819566003500293bdde
Aditya Pratama <pratamaditya7@gmail.com>
2020-10-18 20:54:37 +0800

    🍱 update jquery

--- resources/views/main/layouts/default.blade.php
+++ resources/views/main/layouts/default.blade.php
@@ -120,7 +120,9 @@
 	<!-- Demo Menu -->
 
 	<!-- JavaScripts -->
-	<script src="{{ asset('assets/main/js/jquery-3.4.1.min.js') }}"></script>
+	{{-- <script src="{{ asset('assets/main/js/jquery-3.4.1.min.js') }}"></script> --}}
+	<script src="https://code.jquery.com/jquery-3.5.1.min.js"
+		integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 	<script src="{{ asset('assets/main/js/plugins.min.js') }}"></script>
 	<script src="{{ asset('assets/main/js/common.js') }}"></script>
 	<script src="{{ asset('assets/main/demo/plugins-demo.js') }}"></script>
